cea94639f10eae0d5a7fffbb0c4f2c527280117e
MITUL PATEL <Mitul2711>
2023-09-26 22:38:46 +0530

    changes in footer

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function myFunction(event) {
 
 
 var typed = new Typed(".typing",{
-  strings : ["Web Designer.", "Web Developer.", "Freelancer.", "Student."],
+  strings : ["Web Designer", "Web Developer", "Freelancer", "Student"],
   typeSpeed : 80,
   backSpeed : 80,
 });
@@ -43,3 +43,91 @@ window.addEventListener("scroll", function() {
     }
   }
 });
+
+
+
+// edit by jbodn
+function show(){
+  document.querySelector('.hamburger').classList.toggle('open')
+  document.querySelector('.navigation').classList.toggle('active')
+}
+
+
+//download cv
+
+function downloadCV() {
+  var link = document.createElement('a');
+  link.href = 'port.pdf'; // Replace with the actual URL of your resume file
+  link.download = 'Mitulgondaliya_CV.pdf'; // Replace with the desired file name 
+  link.click();
+}
+
+//changable title
+
+let docTitle = document.title;
+let alertShow = false;
+
+window.addEventListener("blur", () => {
+    document.title = "Hey! See You Soon :(";
+});
+
+window.addEventListener("focus", () => {
+  document.title = docTitle;
+  clearInterval(intervalId);
+});
+
+
+// load animation
+
+var loader = document.getElementById("prealoade");
+
+window.addEventListener("load", function(){
+loader.style.display = "none";
+
+})
+
+
+
+//enter element in site enimation
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry)=> {
+      console.log(entry)
+      if(entry.isIntersecting){
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show')
+      }
+
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+//ghost input feald
+
+const input = document.getElementById('ghost-input');
+const bubble = document.getElementById('ghost-bubble');
+
+const mouth = document.querySelector('.ghost__mouth');
+
+input.onkeydown = e => {
+  if (e.keyCode == 13) {
+    bubble.innerText = e.target.value;
+    e.target.value = '';
+
+    // mouth chatter
+    let i = 0;
+    if (mouthChatter) clearInterval(mouthChatter);
+
+    const mouthChatter = setInterval(() => {
+      mouth.classList.toggle('ghost__mouth--open');
+      if (i === 6) clearInterval(mouthChatter);
+      i++;
+    }, 300);
+  }
+};
+
+
